13ca74507453dc8a91522daccd24261088182c21
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2024-03-18 03:26:29 +0100

    More APIs

---
+++ Neon/ServiceApis/BeatLeader.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Text.Json;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+
+namespace Neon.ServiceApis
+{
+    internal class BeatLeader
+    {
+        String RankedEndpoint = "https://api.beatleader.xyz/playlists";
+
+        // Ranked beatmaps
+        public static JsonObject GetRankedBeatmaps()
+        {
+            JsonObject json = new JsonObject();
+            // fetch the json from the endpoint
+            var client = new HttpClient();
+        }
+    }
+}
